3c4feeba7ab72265c165e795f13b7c22e36ce7ae
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-18 14:41:01 -0400

    styles app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Player from "./components/Player";
 import Board from "./components/Board";
 import './App.css';
+import "./style.css";
 
 function App() {
   return (
