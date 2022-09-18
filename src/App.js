e3fbbbc2465edfb085d9aa7ac06e5de11fa1718c
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-18 14:28:32 -0400

    adds board component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Header from "./components/Header";
 import Player from "./components/Player";
+import Board from "./components/Board";
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Header/>
     <Player whichPlayer='x'/>
     <Player whichPlayer='o'/>
+    <Board/>
     </div>);
  }
 
